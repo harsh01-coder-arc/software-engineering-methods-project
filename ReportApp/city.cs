2b54a9d1d466161b224fed23697ad0956ad20549
Obaidur-421598 <rahadr1742@gmail.com>
2026-05-19 23:10:07 +0100

    Refactor City class properties
    Removed CountryId property and added CountryCode and District properties.

--- ReportApp/city.cs
+++ ReportApp/city.cs
@@ -3,10 +3,8 @@ namespace ReportApp.Models;
 public class City
 {
     public int Id { get; set; }
-
-    public int CountryId { get; set; }
-
-    public string Name { get; set; } = "";
-
+    public string Name { get; set; }
+    public string CountryCode { get; set; }
+    public string District { get; set; }
     public long Population { get; set; }
 }
